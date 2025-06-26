326614a974710075dec267a92884bf92dd4465b5
fsociety00d4t <58801059+fsociety00d4t@users.noreply.github.com>
2025-06-26 19:19:12 +0300

    hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,48 +2,27 @@ import {  useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { LoremIpsum } from "lorem-ipsum";
 
+
 const lorem = new LoremIpsum();
 
 function Header() {
 
 
   return (
-    <>
-      <div id='about'>
-        <h1 className='text-5xl'>About</h1>
-        <p>{lorem.generateParagraphs(5)}</p>
-    </div>
-
-       <div id='features'>
-        <h1 className='text-5xl'>features</h1>
-        <p>{lorem.generateParagraphs(5)}</p>
-    </div>
-
-     <div id='testimonials'>
-        <h1 className='text-5xl'>testimonials</h1>
-        <p>{lorem.generateParagraphs(5)}</p>
-    </div>
-
-     <div id='pricing'>
-        <h1 className='text-5xl'>pricing</h1>
-        <p>{lorem.generateParagraphs(5)}</p>
-    </div>
-
-     <div id='faq'>
-        <h1 className='text-5xl'>faq</h1>
-        <p>{lorem.generateParagraphs(5)}</p>
-    </div>
-
-     <div id='contact'>
-        <h1 className='text-5xl'>contact</h1>
-        <p>{lorem.generateParagraphs(50)}</p>
-    </div>
-
-
-    </>
-  
-
-    
+    <div id="hero"
+    className={`bg-[url('/heroSM.webp')] md:bg-[url('/heroLG.webp')] bg-cover bg-center
+    w-full min-h-[60vh] md:min-h-[80vh]
+    flex flex-col items-center justify-center gap-4 text-white text-center px-4`}
+    >
+      <h1 className='font-inter text-[clamp(1.8rem,5vw,3.5rem)] font-bold text-white'>Unlock Your Potential with Fitness Coach</h1>
+      <h2 className='font-inter text-[clamp(1.2rem,3vw,2rem)] font-semibold text-white mb-4'>Personalized fitness plans to help you reach your goals</h2>
+      <button>
+        <a href="" className='font-inter bg-bg text-textDark font-semibold text-center 
+        py-4 px-8 rounded-md shadow-md 
+        hover:bg-hover hover:text-white active:bg-hover active:text-white 
+        transition-all duration-300 ease-in-out'>Book a Free Call</a>
+      </button>
+    </div>  
   )
 }
 
